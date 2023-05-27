9c9989d10d91380395a70a48f64356f37e069f15
Andy Horn <andyjhorn@gmail.com>
2023-05-27 12:28:10 -0500

    chore: upgrade Dart and Flutter

--- lib/flutter_date_interval.dart
+++ lib/flutter_date_interval.dart
@@ -1,4 +1,4 @@
 library flutter_date_interval;
 
-export './src/date_interval.dart' show DateInterval;
+export 'src/date_interval.dart' show DateInterval;
 export 'src/intervals.dart' show Intervals;
